444fcc06df55cecb8b53e693a39b254b329332eb
Danny Piper <djpiper28@users.noreply.github.com>
2017-07-20 19:56:41 +0100

    Impeoved Hashing a Lot

--- hashcreator.py
+++ hashcreator.py
@@ -23,19 +23,25 @@ $$ |  $$ |      $$ |$$ |  $$ |$$ |$$ |  $$ |$$   ____|$$ |      $$ |      $$ /  
           \$$$$$$  |$$ |          $$ |                                              
            \______/ \__|          \__|                                              """)
 while 1:
-	text = cipher.makeOnlyAlpha(input("Text to hash.\n"))
+	text = cipher.makeOnlyAlpha(input("Text to hash.\n").lower())
 	while (text == None):
 		text = cipher.makeOnlyAlpha(input("Text to hash.\n").lower())
 	i=0#used in a loop
+	text = "a"+text#Hides first letter of text
 	output=[]
 	while (i<len(text)):
-		out = hash(cipher.getnum(text[i])+int(cipher.getnum(text[i])*(len(text)+(cipher.getnum(text[i])/2))*(i/cipher.getnum(text[i]))+i)+cipher.getnum(text[i]))
+		out = 16+hash(cipher.getnum(text[i])+int(cipher.getnum(text[i])*(len(text)+(cipher.getnum(text[i])/2))*(i/cipher.getnum(text[i]))+i)+cipher.getnum(text[i])*((i+5)*len(text)**2))
 		while (out<1):
-			out=out+(len(text)*(i+1))+2+len(text)
+			out=out+(len(text)*(i+1))**2+len(text)+2*(cipher.getnum(text[0]**2))
 		while (out>26):
-			out = out - 26	
+			if(out<=100):
+				out = out - 26	
+			elif(out>100 and out<500):
+				out=out-50
+			else:
+				out=out-100
 		if(out==None):
 			out=out+(len(text)*(i+1))+2
 		output.append(cipher.getletter(out))
 		i=i+1
-	print("".join(output))
+	print("\n"+"".join(output))
